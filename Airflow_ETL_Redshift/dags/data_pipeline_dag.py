3c583751bff3056977378e803c56355f0b85bdbd
Christopher Ozurumba <jaycees20@yahoo.com>
2025-05-05 14:11:34 +0100

    update stage events

--- Airflow_ETL_Redshift/dags/data_pipeline_dag.py
+++ Airflow_ETL_Redshift/dags/data_pipeline_dag.py
@@ -1,11 +1,14 @@
 from datetime import datetime, timedelta
 import os
-from airflow import DAG
+from airflow import DAG # type: ignore
 from airflow.operators.empty import EmptyOperator
-# from airflow.operators.dummy import DummyOperator
 # from airflow.operators.postgres_operator import PostgresOperator
-from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+# from airflow.legacy.providers.postgres.operators.postgres import PostgresOperator
+# from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.amazon.aws.operators.redshift_data import RedshiftDataOperator
 
+import sql_scripts
 
 default_args = {
     'owner': 'mamiololo',
@@ -27,10 +30,45 @@ dag = DAG('data_pipeline_dag',
 
 start_operator = EmptyOperator(task_id='Begin_execution', dag=dag)
 
-create_staging_events_table = PostgresOperator(
+create_staging_events_table = SQLExecuteQueryOperator(
     task_id='create_staging_events_table',
     dag=dag,
-    postgres_conn_id='redshift',
-    sql=sql_statements.CREATE_staging_events_TABLE_SQL
+    conn_id='redshift',
+    sql=sql_scripts.CREATE_staging_events_TABLE_SQL
+)
+
+stage_events_to_redshift = RedshiftDataOperator(
+    task_id='Stage_events_from_s3_to_redshift',
+    dag=dag,
+    database='dev',
+    sql=f"""
+        COPY staging_events
+        FROM 's3://mamiololo-storage/log_data' 
+        CREDENTIALS 'aws_iam_role=arn:aws:iam::YOUR_ACCOUNT_ID:role/ROLE_NAME' 
+        JSON 's3://mamiololo-storage/log_json_path.json' 
+    """,
+    aws_conn_id='aws_credentials',
+    region_name='us-west-2'
+)
+
+create_staging_songs_table = SQLExecuteQueryOperator(
+    task_id="create_staging_songs_table",
+    dag=dag,
+    conn_id="redshift",
+    sql=sql_scripts.CREATE_staging_songs_TABLE_SQL
+)
+
+stage_songs_to_redshift = RedshiftDataOperator(
+    task_id='Stage_songs_from_s3_to_redshift',
+    dag=dag,
+    database='dev',
+    sql=f"""
+        COPY staging_events
+        FROM 's3://mamiololo-storage/log_data' 
+        CREDENTIALS 'aws_iam_role=arn:aws:iam::YOUR_ACCOUNT_ID:role/ROLE_NAME' 
+        JSON 's3://mamiololo-storage/log_json_path.json' 
+    """,
+    aws_conn_id='aws_credentials',
+    region_name='us-west-2'
 )
 
